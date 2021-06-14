219a581b7f0d5999402b530793b6405579126613
komal kumari <komalsah342@gmail.com>
2021-06-14 15:43:42 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <>
+        <header className="header">
+            <div>
+            <h1>ALL COUNTRIES</h1>
+            </div>
+
+            <div>
+                <i className="fas fa-moon"></i>
+            </div>
+        </header>
+            
+        </>
+    )
+}
+
+export default Header
